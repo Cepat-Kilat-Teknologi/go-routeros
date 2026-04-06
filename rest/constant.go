d15e84d11206436f620a31de6b5dbdf3f922fa57
sumitroajiprabowo <bowo@anakdesa.id>
2026-04-06 20:31:19 +0700

    feat(rest): add HTTP method and protocol constants

---
+++ rest/constant.go
@@ -0,0 +1,13 @@
+// rest/constant.go
+package rest
+
+const (
+	httpProtocol        = "http"
+	httpsProtocol       = "https"
+	MethodGet           = "GET"
+	MethodPost          = "POST"
+	MethodPut           = "PUT"
+	MethodPatch         = "PATCH"
+	MethodDelete        = "DELETE"
+	tlsHandshakeFailure = "tls: handshake failure"
+)
